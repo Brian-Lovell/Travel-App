d66e9ba3f7f17812a7b06c7941d3267d0a397998
Brian Lovell <brian.lovell@outlook.com>
2021-08-04 12:08:21 -0400

    Comment out old API add new variables

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,27 +4,30 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // Check what was entered into text field
-    let formText = document.getElementById('name').value
-    if (checkForName(formText)) {
-        console.log("::: Form Submitted :::")
-        fetch('http://localhost:2076/analyze', {
-            method: 'POST',
-            credentials: 'same-origin',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({url: formText})
-        })
-        .then(res => res.json())
-        .then(function(res) {
-            document.getElementById('score').innerHTML = res.score
-            document.getElementById('agreement').innerHTML = res.agreement
-            document.getElementById('subjectivity').innerHTML = res.subjectivity
-            document.getElementById('confidence').innerHTML = res.confidence
-            document.getElementById('irony').innerHTML = res.irony
-        })
-    }
+    let formDate = document.getElementById('tripDate').value
+    let formLocation = document.getElementById('location').value
+    console.log(formDate)
+    console.log(formLocation)
+    // if (checkForName(formText)) {
+    //     console.log("::: Form Submitted :::")
+    //     fetch('http://localhost:2076/analyze', {
+    //         method: 'POST',
+    //         credentials: 'same-origin',
+    //         mode: 'cors',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({url: formText})
+    //     })
+    //     .then(res => res.json())
+    //     .then(function(res) {
+    //         document.getElementById('score').innerHTML = res.score
+    //         document.getElementById('agreement').innerHTML = res.agreement
+    //         document.getElementById('subjectivity').innerHTML = res.subjectivity
+    //         document.getElementById('confidence').innerHTML = res.confidence
+    //         document.getElementById('irony').innerHTML = res.irony
+        // })
+    // }
 }
 
 export { handleSubmit }
