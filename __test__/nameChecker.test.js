28a1b2ad96a6d6a82982b9dad4151ace357ea0e6
Brian Lovell <brian.lovell@outlook.com>
2021-08-11 10:39:48 -0400

    Remove unused test

--- __test__/nameChecker.test.js
+++
@@ -1,7 +0,0 @@
-import  {checkForName} from '../src/client/js/nameChecker'
-
-describe("Test the check for name function", () => {
-    test("Test the checkForName() function", () => {
-        expect(checkForName).toBeTruthy();
-    })
-})
