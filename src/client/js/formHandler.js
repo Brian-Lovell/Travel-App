18fdf473e09ff20b9bc64c007fa4baa5d35303d8
Brian Lovell <brian.lovell@outlook.com>
2021-08-11 10:24:41 -0400

    Add countdown script

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -22,6 +22,28 @@ function handleSubmit(event) {
     .then (res => res.json())
     .then (res => {
         console.log(res)
+        // Add countdown
+        let countDownDate = new Date(formDate).getTime()
+        let x = setInterval(function() {
+            let now = new Date().getTime()
+
+            let distance = countDownDate - now
+
+            let days = Math.floor(distance / (1000 * 60 * 60 *24))
+
+            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+
+            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+
+            let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+            document.getElementById("countdown").innerHTML = days + "d" + hours + "h " + minutes + "m " + seconds + "s "
+
+            if (distance < 0) {
+                clearInterval(x)
+                document.getElementById("countdown").innerHTML = "Today is the day!"
+            }
+        }, 1000)
 
         // Add travel location image
         const img = document.getElementById('resultsImage')
